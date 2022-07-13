049045e6721ab34360ef6a6c8cab6aa7b67016d0
Alvaro Ortiz <varortz@gmail.com>
2022-07-12 21:21:06 -0700

    can-change-canvas-color-

--- public/js/my-drawing-app.js
+++ public/js/my-drawing-app.js
@@ -12,11 +12,13 @@ tool.onMouseDown = function(event) { //This code in this function is called when
     path.strokeWidth = currentWidth;
     console.log(event.point); //this commands log to the Console the coordinates of the mouse click. Feel free to delete it!
 }
+
 tool.onMouseDrag = function(event) {
     path.add(event.point); //Add points to the path as the user drags their mouse.
 }
 
-$('#default').on('click', function (e) { //jquery click event code for our "pencil" button.
+$('#brush').on('click', function (e) { //jquery click event code for our "pencil" button.
+  console.log('brush')
     currentColor = 'black';  //change the color to black
     currentWidth = 5;       //change the width to 5
 })
@@ -28,4 +30,24 @@ $('#thick-green').on('click', function (e) { //jquery button click code for our 
 $('#eraser').on('click', function (e) { //jquery button click code for our eraser button.
     currentColor = 'white';
     currentWidth = 20;
+})
+
+$('#bChalk').on('click', function (e) {
+  console.log('clicked-bChalk')
+  $('#my-canvas')
+          .toggleClass('chalkboard-b')
+
+
+})
+
+$('#gChalk').on('click', function (e) {
+  console.log('green')
+  $('#my-canvas')
+          .toggleClass('chalkboard-g')
+})
+
+$('mode').on('click', function (e) {
+  console.log('green')
+  $('#my-canvas')
+          .toggleClass('chalkboard-b')
 })
